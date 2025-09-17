cf50e27fb165a0569328fbc57119b53d663fba91
Bettrix <bettbrian022@gmail.com>
2025-09-18 00:21:09 +0300

    saturdayFun function with activity overide built in

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-function saturdayFun(activity="roller-skate"){
-    let result = `This Saturday, I want to ${activity}`;
-    console.log(result);
+function saturdayFun(activity = "roller-skate"){
+    let result = `This Saturday, I want to ${activity}!`;
+    return result;
+    // console.log(result);
 }
 
-saturdayFun("swim");
+saturdayFun();
+
+const fn = function() {
+  console.log("Yet more razzling");
+}
+
+console.log(fn);
+fn();
+
+const mondayWork = function(activity = "go to the office") {
+    console.log(`This Monday, I will ${activity}`);
+}
+
+mondayWork("go home and sleep");
