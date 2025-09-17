0f1ba498b0cebd5ef071cb8e873d938c8d1c5888
Bettrix <bettbrian022@gmail.com>
2025-09-17 23:56:56 +0300

    defining saturdayFun function as required with default parameter value set to 'roller-skate'

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    let result = `This Saturday, I want to ${activity}`;
+    console.log(result);
+}
+
+saturdayFun("swim");
